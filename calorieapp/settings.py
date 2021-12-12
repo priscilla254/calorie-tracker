3021254a88051b2bf0ce75e2a99b259d308f2f09
priscilla254 <priscillawambui8@gmail.com>
2021-12-12 16:49:11 +0300

    created a .env file and gitignore

--- calorieapp/settings.py
+++ calorieapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-84a$f^cg=*yyhp13*t44p)k3&9wae68-v65k87j1bvpa!pmff4'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
